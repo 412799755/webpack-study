f4f98aff734638b21fe1d062e6fa7206d692b7a3
luoy <412799755@qq.com>
2019-06-19 18:13:06 +0800

    canvas drawImage

--- src/index.js
+++ src/index.js
@@ -15,3 +15,49 @@ function component() {
     return element;
 }
 document.body.appendChild(component());
+function img(){
+    const element = document.createElement('div')
+    var canvas = document.createElement('canvas')
+    var canvas2 = document.createElement('canvas')
+    const image = new Image();
+
+    image.crossOrigin = '';
+    image.onload = ()=>{
+        console.log(image.width)
+        const ctx = canvas.getContext("2d")
+        const ctx2 = canvas2.getContext("2d")
+        canvas.width = image.width;
+        canvas.height = image.height;
+        ctx.drawImage(image, 0, 0);
+        canvas.onmousemove = function(e){
+            var mouseX,mouseY
+            if(e.offsetX){
+                mouseX = e.offsetX;
+                mouseY = e.offsetY;
+            }else{
+                mouseX = e.layerX;
+                mouseY = e.layerY;
+            }
+           console.log(ctx.getImageData(mouseX, mouseY, 1, 1).data);
+        }
+        canvas2.width = image.width;
+        canvas2.height = image.height/3;
+        element.appendChild(canvas);
+        // element.appendChild(canvas2);
+        for (let j = 0; j < 3; j++) {
+            ctx2.drawImage(image,0,-j*image.height/3,image.width, image.height);
+            var i = new Image()
+            i.src = canvas2.toDataURL()
+            element.appendChild(i);
+        }
+    }
+    image.src = 'https://img-blog.csdn.net/20160608095321599'
+
+
+
+
+
+    element.appendChild(image);
+    return element
+}
+document.body.appendChild(img());
--- src/print.js
+++ src/print.js
@@ -1,4 +1,4 @@
 
        console.log('I get called from print.js!');
-      cosnole.log('I get called from print.js!');
+      // cosnole.log('I get called from print.js!');
 
